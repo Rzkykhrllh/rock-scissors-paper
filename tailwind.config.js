c9f1cb5f4102106da65815c707ac336d14f1ed26
Airu <rizkykirigaya@gmail.com>
2022-09-06 14:57:27 +0700

    create score component ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(circle, #1f3756 50%, #141539)',
+      }
+    }
   },
   plugins: [],
 }
